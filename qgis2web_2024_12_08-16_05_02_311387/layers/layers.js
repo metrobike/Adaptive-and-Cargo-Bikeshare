63c9c326160de3214cfb61c4cd8eb8001e4db064
metrobike <apartmentsearch418@gmail.com>
2024-12-08 16:08:38 -0500

    Add files via upload

---
+++ qgis2web_2024_12_08-16_05_02_311387/layers/layers.js
@@ -0,0 +1,59 @@
+var wms_layers = [];
+
+
+        var lyr_Basemap_0 = new ol.layer.Tile({
+            'title': 'Basemap',
+            'type':'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' &middot; <a href="https://cartodb.com/basemaps/">Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.</a>',
+                url: 'https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}@2x.png'
+            })
+        });
+var format_CountyBoundary_1 = new ol.format.GeoJSON();
+var features_CountyBoundary_1 = format_CountyBoundary_1.readFeatures(json_CountyBoundary_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_CountyBoundary_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_CountyBoundary_1.addFeatures(features_CountyBoundary_1);
+var lyr_CountyBoundary_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_CountyBoundary_1, 
+                style: style_CountyBoundary_1,
+                popuplayertitle: "County Boundary",
+                interactive: false,
+                title: '<img src="styles/legend/CountyBoundary_1.png" /> County Boundary'
+            });
+var format_PotentialLocation_2 = new ol.format.GeoJSON();
+var features_PotentialLocation_2 = format_PotentialLocation_2.readFeatures(json_PotentialLocation_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_PotentialLocation_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_PotentialLocation_2.addFeatures(features_PotentialLocation_2);
+var lyr_PotentialLocation_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_PotentialLocation_2, 
+                style: style_PotentialLocation_2,
+                popuplayertitle: "Potential Location",
+                interactive: true,
+    title: 'Potential Location<br />\
+    <img src="styles/legend/PotentialLocation_2_0.png" /> Phase 1<br />\
+    <img src="styles/legend/PotentialLocation_2_1.png" /> Phase 2<br />\
+    <img src="styles/legend/PotentialLocation_2_2.png" /> Phase 3 (Optional)<br />'
+        });
+
+lyr_Basemap_0.setVisible(true);lyr_CountyBoundary_1.setVisible(true);lyr_PotentialLocation_2.setVisible(true);
+var layersList = [lyr_Basemap_0,lyr_CountyBoundary_1,lyr_PotentialLocation_2];
+lyr_CountyBoundary_1.set('fieldAliases', {'fid': 'fid', 'OBJECTID': 'OBJECTID', 'COUNTY': 'COUNTY', 'STATE': 'STATE', 'Shape__Area': 'Shape__Area', 'Shape__Length': 'Shape__Length', });
+lyr_PotentialLocation_2.set('fieldAliases', {'OBJECTID': 'OBJECTID', 'CODE': 'CODE', 'NAME': 'NAME', 'SYMBOL': 'SYMBOL', 'SENIORCENTER': 'SENIORCENTER', 'ADDRESS': 'ADDRESS', 'GeoSyncDate': 'GeoSyncDate', 'Senior Center': 'Senior Center', 'Location Type': 'Location Type', 'Pilot Location': 'Pilot Phase', 'Location Number': 'Location Number', });
+lyr_CountyBoundary_1.set('fieldImages', {'fid': 'Range', 'OBJECTID': 'Range', 'COUNTY': 'TextEdit', 'STATE': 'TextEdit', 'Shape__Area': 'TextEdit', 'Shape__Length': 'TextEdit', });
+lyr_PotentialLocation_2.set('fieldImages', {'OBJECTID': 'TextEdit', 'CODE': 'ValueMap', 'NAME': 'TextEdit', 'SYMBOL': 'ValueMap', 'SENIORCENTER': 'TextEdit', 'ADDRESS': 'TextEdit', 'GeoSyncDate': 'DateTime', 'Senior Center': 'TextEdit', 'Location Type': 'TextEdit', 'Pilot Location': 'TextEdit', 'Location Number': 'Range', });
+lyr_CountyBoundary_1.set('fieldLabels', {'fid': 'hidden field', 'OBJECTID': 'hidden field', 'COUNTY': 'hidden field', 'STATE': 'hidden field', 'Shape__Area': 'hidden field', 'Shape__Length': 'hidden field', });
+lyr_PotentialLocation_2.set('fieldLabels', {'OBJECTID': 'hidden field', 'CODE': 'hidden field', 'NAME': 'inline label - visible with data', 'SYMBOL': 'hidden field', 'SENIORCENTER': 'hidden field', 'ADDRESS': 'inline label - visible with data', 'GeoSyncDate': 'hidden field', 'Senior Center': 'inline label - visible with data', 'Location Type': 'inline label - visible with data', 'Pilot Location': 'inline label - visible with data', 'Location Number': 'hidden field', });
+lyr_PotentialLocation_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ qgis2web_2024_12_08-16_05_02_311387/styles/PotentialLocation_2_style.js
@@ -0,0 +1,60 @@
+var size = 0;
+var placement = 'point';
+function categories_PotentialLocation_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'Phase 1':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,87,97,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Phase 2':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(255,219,121,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Phase 3 (Optional)':
+                    return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(25,167,255,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_PotentialLocation_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("Pilot Location");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.4px \'Arial\', sans-serif";
+    var labelFill = "#8d8d8d";
+    var bufferColor = "#ffffff";
+    var bufferWidth = 1.75;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Location Number") !== null) {
+        labelText = String(feature.get("Location Number"));
+    }
+    
+var style = categories_PotentialLocation_2(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
